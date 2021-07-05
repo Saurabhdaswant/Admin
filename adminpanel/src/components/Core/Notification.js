d954157fa967889dc179b8087a604ead6aea3c6a
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-07-05 13:24:14 +0530

    fix : notifications heading
    heading style changed acording to figma file

--- adminpanel/src/components/Core/Notification.js
+++ adminpanel/src/components/Core/Notification.js
@@ -8,7 +8,7 @@ const Notification = () => {
       <div className="main-child-two">
         <div className="head">
           <img src="svg/Bell.svg" alt="" />
-          <h3>Notifications</h3>
+          <p>Notifications</p>
         </div>
 
         <h3 className="day">Today</h3>
