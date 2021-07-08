a4e6d164b8c4000428d141221a9207a005000dd3
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-07-08 12:49:09 +0530

    updated all sections padding and margin

--- adminpanel/src/components/Core/Notification.js
+++ adminpanel/src/components/Core/Notification.js
@@ -6,12 +6,13 @@ const Notification = () => {
   return (
     <div className="notification">
       <div className="main-child-two">
-        <div className="head">
-          <img src="svg/Bell.svg" alt="" />
-          <p>Notifications</p>
+        <div className="theHead">
+          <div className="head">
+            <img src="svg/Bell.svg" alt="" />
+            <p>Notifications</p>
+          </div>
+          <h3 className="day">Today</h3>
         </div>
-
-        <h3 className="day">Today</h3>
         <Switch>
           <Route exact path="/" component={Fut} />
           <Route path="/Fut" component={Fut} />
